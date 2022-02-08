beac5057da248c1d056d080977f1f90a60d6ca65
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-02-08 11:25:27 +0530

    :wrench: database configured

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectToDb = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) =>
+      console.log(`Database connected with ${data.connection.host}`)
+    )
+    .catch((err) => {
+      console.log(`Error: ${err.message}`);
+    });
+};
+
+module.exports = connectToDb;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,35 @@
+// load env-vars
+require('dotenv').config();
+
+// requiring dependencies
 const express = require('express');
 
+// require db configs
+const connectToDb = require('./config/db');
+
+// initialize express
 const app = express();
 
-app.listen(5000, () => {
+// uncaught exception
+process.on('uncaughtException', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Server shutting down due to uncaught exception`);
+  process.exit(1);
+});
+
+// connect to db
+connectToDb();
+
+// starting server
+app.listen(process.env.PORT, () => {
   console.log('server running 🚀');
+});
+
+// unhandled promise rejection
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Server shutting down due to unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
