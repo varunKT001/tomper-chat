debe792a9ff0905db1d9dfa3a7aa014aa87a18b2
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-02-13 17:18:16 +0530

    :bug: cookies bug fix for android chrome

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -6,8 +6,6 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: 'none',
-    secure: true,
   };
   res
     .status(statusCode)
