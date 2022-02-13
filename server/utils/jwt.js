3e6c2fa11b480ebe6af357b6bbc389b37a7b9a65
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-02-13 16:50:30 +0530

    :bug: http only cookies fix

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -5,7 +5,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: false,
+    httpOnly: true,
     sameSite: 'none',
     secure: true,
   };
